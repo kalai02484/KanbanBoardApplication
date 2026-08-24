cb96ee4fda1452d5a57b6e684f35ae233eadc7c5
kalai02484 <kalai02484@gmail.com>
2026-08-25 04:30:52 +0530

    Dnd Draggable added to card

--- src/Components/TaskCard.jsx
+++ src/Components/TaskCard.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { useTasks } from "../context/TaskContext";
+import { useDraggable } from "@dnd-kit/react";
 
 const TaskCard = ({ task }) => {
   const { deleteTask } = useTasks();
+
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: task.id,
+  });
 
   const handleDelete = (id) => {
     event.stopPropagation();
@@ -10,8 +15,20 @@ const TaskCard = ({ task }) => {
     deleteTask(id);
   };
 
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
+
   return (
-    <article className="cursor-grab rounded-md p-4 transition hover:bg-stone-500 active:cursor-grabbing">
+    <article
+      className="cursor-grab rounded-md p-4 transition hover:bg-stone-500 active:cursor-grabbing"
+      ref={setNodeRef}
+      style={style}
+      {...listeners}
+      {...attributes}
+    >
       <div className="flex items-start justify-between gap-3">
         <h3 className="font-bold text-slate-300">{task.title}</h3>
         <button
